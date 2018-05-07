5a48d5c7e8f04ef26d50d3ce4d70937c609201b6
liuweiyin <liuwyfly@hotmail.com>
2018-05-07 21:14:40 +0800

    EditText 样式

--- app/src/main/java/com/dpc/www/mytoolbar/LoginActivity.java
+++ app/src/main/java/com/dpc/www/mytoolbar/LoginActivity.java
@@ -13,9 +13,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setTitle("登录");
+        // mToolbar.setTitle("登录");
+        // mToolbar.setTitleTextColor(Color.WHITE);
         mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
-        mToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
